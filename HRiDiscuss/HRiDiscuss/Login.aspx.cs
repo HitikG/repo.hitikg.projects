9be9565c03e2874469f391fe98a26eb6c3fed4c7
HitikG <hitik@live.co.uk>
2014-07-18 21:38:15 +0100

    V1.0.6

--- HRiDiscuss/HRiDiscuss/Login.aspx.cs
+++ HRiDiscuss/HRiDiscuss/Login.aspx.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -25,11 +28,45 @@ namespace WebShare
             }
             else
             {
+                byte[] Results;
+                System.Text.UTF8Encoding UTF8 = new System.Text.UTF8Encoding();
+
+                // Step 1. We hash the passphrase using MD5 
+                // We use the MD5 hash generator as the result is a 128 bit byte array 
+                // which is a valid length for the TripleDES encoder we use below
+                MD5CryptoServiceProvider HashProvider = new MD5CryptoServiceProvider();
+                byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(TbCreatePass.Text));
+                // Step 2. Create a new TripleDESCryptoServiceProvider object 
+                TripleDESCryptoServiceProvider TDESAlgorithm = new TripleDESCryptoServiceProvider();
+                // Step 3. Setup the encoder 
+                TDESAlgorithm.Key = TDESKey;
+                TDESAlgorithm.Mode = CipherMode.ECB;
+                TDESAlgorithm.Padding = PaddingMode.PKCS7;
+                // Step 4. Convert the input string to a byte[] 
+                byte[] DataToEncrypt = UTF8.GetBytes(TbCreatePass.Text);
+
+                // Step 5. Attempt to encrypt the string 
+                try
+                {
+                    ICryptoTransform Encryptor = TDESAlgorithm.CreateEncryptor();
+                    Results = Encryptor.TransformFinalBlock(DataToEncrypt, 0, DataToEncrypt.Length);
+                }
+                finally
+                {
+                    // Clear the TripleDes and Hashprovider services of any sensitive information 
+                    TDESAlgorithm.Clear();
+                    HashProvider.Clear();
+                }
+                // Step 6. Return the encrypted string as a base64 encoded string 
+                Convert.ToBase64String(Results);
+
                 string AccountQuery = "INSERT INTO Accounts (Username, Password)  VALUES (@Username, @Password)"; //Our Query To Insert
                 SqlConnection AccountConnect = new SqlConnection(ConfigurationManager.ConnectionStrings["DsAccounts"].ConnectionString); //Declaring Our Connection String
                 SqlCommand AccountCmd = new SqlCommand(AccountQuery, AccountConnect); //Create A Command To Add To The Database
                 AccountCmd.Parameters.AddWithValue("@Username", TbCreateUser.Text); //Inserts The Username Into The Database
-                AccountCmd.Parameters.AddWithValue("@Password", TbCreatePass.Text); //Inserts The Password Into The Database
+                AccountCmd.Parameters.AddWithValue("@Password", Results); //Inserts The Password Into The Database
+
+
 
                 if (TbCreateUser.Text == "") //Checks If The Textbox Is Empty
                 {
@@ -55,36 +92,156 @@ namespace WebShare
             }
         }
 
-        protected void BtnLogin_Click(object sender, EventArgs e)
+        /*protected void BtnLogin_Click(object sender, EventArgs e)
         {
-            string LoginQuery = "SELECT * FROM Accounts WHERE Username=@Username AND Password=@Password"; //Our Query To Insert
-            SqlConnection LoginConnect = new SqlConnection(ConfigurationManager.ConnectionStrings["DsAccounts"].ConnectionString); //Declaring Our Connection String
-            SqlCommand LoginCmd = new SqlCommand(LoginQuery, LoginConnect); //Create A Command To Add To The Database
-            LoginCmd.Parameters.AddWithValue("@Username", TbLoginUser.Text); //Stores The Data
-            LoginCmd.Parameters.AddWithValue("@Password", TbLoginPass.Text); //Stores The Data
+            byte[] Results;
+            System.Text.UTF8Encoding UTF8 = new System.Text.UTF8Encoding();
 
-            LoginConnect.Open(); //Open The Connection
-            SqlDataReader Reader = LoginCmd.ExecuteReader(); //Reads The Database
-            int counter = 0; //Declaring Our Int
+            // Step 1. We hash the passphrase using MD5 
+            // We use the MD5 hash generator as the result is a 128 bit byte array 
+            // which is a valid length for the TripleDES encoder we use below
+            MD5CryptoServiceProvider HashProvider = new MD5CryptoServiceProvider();
+            byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(TbLoginPass.Text));
+            // Step 2. Create a new TripleDESCryptoServiceProvider object 
+            TripleDESCryptoServiceProvider TDESAlgorithm = new TripleDESCryptoServiceProvider();
+            // Step 3. Setup the encoder 
+            TDESAlgorithm.Key = TDESKey;
+            TDESAlgorithm.Mode = CipherMode.ECB;
+            TDESAlgorithm.Padding = PaddingMode.PKCS7;
+            // Step 4. Convert the input string to a byte[] 
+            byte[] DataToEncrypt = UTF8.GetBytes(TbLoginPass.Text);
 
-            while (Reader.Read()) //While It Reads
+            // Step 5. Attempt to encrypt the string 
+            try
             {
-                counter++; //Checks The Database
+                ICryptoTransform Encryptor = TDESAlgorithm.CreateEncryptor();
+                Results = Encryptor.TransformFinalBlock(DataToEncrypt, 0, DataToEncrypt.Length);
+            }
+            finally
+            {
+                // Clear the TripleDes and Hashprovider services of any sensitive information 
+                TDESAlgorithm.Clear();
+                HashProvider.Clear();
+            }
+            // Step 6. Return the encrypted string as a base64 encoded string 
+            string based = Convert.ToBase64String(Results);
+
+            string LoginQuery = "SELECT * FROM Accounts WHERE Username=@Username Password=@Password"; //Our Query String
+            SqlConnection LoginConnect = new SqlConnection(ConfigurationManager.ConnectionStrings["DsAccounts"].ConnectionString); //Declaring Our Connection
+            SqlCommand LoginCmd = new SqlCommand(LoginQuery, LoginConnect); //Declaring Our Command
+
+            string User = TbLoginUser.Text;
+            string Pass = TbLoginPass.Text;
+            byte[] b = Convert.FromBase64String(based);
+
+            based = System.Text.Encoding.UTF8.GetString(b);
+
+            if (Results == b)
+            {
+                bool treu = true;
+                LblLoginMsg.Text = "YES";
             }
 
-            LoginConnect.Close(); //Close Our Connection 
-
-            if (counter > 0) //If The User Exists
-            {
-                Session["LoggedIn"] = "true"; //They're Logged In
-                Session["Username"] = TbLoginUser.Text; //Displays The User's Username
-                Response.Redirect("Default.aspx"); //Redirect
-            }
             else
             {
-                LblLoginMsg.Text = "Login Failed, Enter Valid Credentials."; //Logon Failed 
+                LblLoginMsg.Text = "FAIL";
             }
-        }
+        }*/
+
+        protected void BtnLogin_Click(object sender, EventArgs e)
+         {
+             byte[] Results;
+             System.Text.UTF8Encoding UTF8 = new System.Text.UTF8Encoding();
+
+             // Step 1. We hash the passphrase using MD5 
+             // We use the MD5 hash generator as the result is a 128 bit byte array 
+             // which is a valid length for the TripleDES encoder we use below
+             MD5CryptoServiceProvider HashProvider = new MD5CryptoServiceProvider();
+             byte[] TDESKey = HashProvider.ComputeHash(UTF8.GetBytes(TbCreatePass.Text));
+             // Step 2. Create a new TripleDESCryptoServiceProvider object 
+             TripleDESCryptoServiceProvider TDESAlgorithm = new TripleDESCryptoServiceProvider();
+             // Step 3. Setup the encoder 
+             TDESAlgorithm.Key = TDESKey;
+             TDESAlgorithm.Mode = CipherMode.ECB;
+             TDESAlgorithm.Padding = PaddingMode.PKCS7;
+             // Step 4. Convert the input string to a byte[] 
+             byte[] DataToEncrypt = UTF8.GetBytes(TbCreatePass.Text);
+
+             // Step 5. Attempt to encrypt the string 
+             try
+             {
+                 ICryptoTransform Encryptor = TDESAlgorithm.CreateEncryptor();
+                 Results = Encryptor.TransformFinalBlock(DataToEncrypt, 0, DataToEncrypt.Length);
+             }
+             finally
+             {
+                 // Clear the TripleDes and Hashprovider services of any sensitive information 
+                 TDESAlgorithm.Clear();
+                 HashProvider.Clear();
+             }
+             // Step 6. Return the encrypted string as a base64 encoded string 
+             Convert.ToBase64String(Results);
+
+             byte[] Results;
+             System.Text.UTF8Encoding UTrF8 = new System.Text.UTF8Encoding();
+             // Step 1. We hash the passphrase using MD5 
+             // We use the MD5 hash generator as the result is a 128 bit byte array 
+             // which is a valid length for the TripleDES encoder we use below
+             MD5CryptoServiceProvider HashPrrovider = new MD5CryptoServiceProvider();
+             byte[] TDrESKey = HashProvider.ComputeHash(UTF8.GetBytes(TbLoginPass.Text));
+             // Step 2. Create a new TripleDESCryptoServiceProvider object 
+             TripleDESCryptoServiceProvider TDESrAlgorithm = new TripleDESCryptoServiceProvider();
+             // Step 3. Setup the decoder 
+             TDESAlgorithm.Key = TDESKey;
+             TDESAlgorithm.Mode = CipherMode.ECB;
+             TDESAlgorithm.Padding = PaddingMode.PKCS7;
+            // Step 4. Convert the input string to a byte[] 
+             byte[] DataToDecrypt = Convert.FromBase64String(Results);
+
+             // Step 5. Attempt to decrypt the string 
+             try
+             {
+                 ICryptoTransform Decryptor = TDESAlgorithm.CreateDecryptor();
+                 Results = Decryptor.TransformFinalBlock(DataToDecrypt, 0, DataToDecrypt.Length);
+             }
+             finally
+             {
+                 // Clear the TripleDes and Hashprovider services of any sensitive information 
+                 TDESAlgorithm.Clear();
+                 HashProvider.Clear();
+             }
+             // Step 6. Return the decrypted string in UTF8 format 
+             UTF8.GetString(Results);
+
+             string LoginQuery = "SELECT * FROM Accounts WHERE Username=@Username AND Password=@Password"; //Our Query To Insert
+             SqlConnection LoginConnect = new SqlConnection(ConfigurationManager.ConnectionStrings["DsAccounts"].ConnectionString); //Declaring Our Connection String
+             SqlCommand LoginCmd = new SqlCommand(LoginQuery, LoginConnect); //Create A Command To Add To The Database
+             LoginCmd.Parameters.AddWithValue("@Username", TbLoginUser.Text); //Stores The Data
+             LoginCmd.Parameters.AddWithValue("@Password", Results); //Stores The Data
+
+             LoginConnect.Open(); //Open The Connection
+             SqlDataReader Reader = LoginCmd.ExecuteReader(); //Reads The Database
+             int counter = 0; //Declaring Our Int
+
+
+             while (Reader.Read()) //While It Reads
+             {
+                 counter++; //Checks The Database
+             }
+
+             LoginConnect.Close(); //Close Our Connection 
+
+             if (counter > 0) //If The User Exists
+             {
+                 Session["LoggedIn"] = "true"; //They're Logged In
+                 Session["Username"] = TbLoginUser.Text; //Displays The User's Username
+                 Response.Redirect("Default.aspx"); //Redirect
+             }
+             else
+             {
+                 LblLoginMsg.Text = "Login Failed, Enter Valid Credentials."; //Logon Failed 
+             }
+         }
 
         public bool CheckUser(string Username) //Checks If The User Exists
         {
@@ -113,6 +270,29 @@ namespace WebShare
             }
         }
 
+        /*public static  string GetPassword(string Username, string Password)
+        {
+            string LoginQuery = "SELECT * FROM Accounts WHERE Username=@Username Password=@Password"; //Our Query String
+            SqlConnection LoginConnect = new SqlConnection(ConfigurationManager.ConnectionStrings["DsAccounts"].ConnectionString); //Declaring Our Connection
+            SqlCommand LoginCmd = new SqlCommand(LoginQuery, LoginConnect); //Declaring Our Command
+            LoginCmd.Parameters.AddWithValue("@Username", TbLoginUser.Text); //Stores Our Data
+            LoginCmd.Parameters.AddWithValue("@Password", TbLoginPass.Text); //Stores Our Data
+
+            int UserCount = 0; //Declaring Our Int
+            LoginConnect.Open(); //Opens Our Connection
+            SqlDataReader CheckReader = LoginCmd.ExecuteReader(); //Opens Our Reader
+
+            while (CheckReader.Read()) //While It Reads
+            {
+                UserCount++; //Checks
+            }
+            LoginConnect.Close();
+
+            return Username;
+            return Password;
+        }*/
     }
 
 }
+
+
--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 repo.hitikg.projects
 ====================
 
-This Is Where All The Projects For HRiUtilities Are Stored. They Are All Copyrighted By Hitik Gohil.
+This Is Where All The Projects For HRiUtilities Are Stored. They Are All Copyrighted By Hitik Gohil.   
  
